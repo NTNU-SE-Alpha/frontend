8e5dfe090ac6ec21f4fd3ab6f42478a1eaa248d4
Danny Hsu <danny539425@gmail.com>
2024-10-17 01:08:14 +0800

    course update

--- my-app/src/Components/CourseCard.js
+++ my-app/src/Components/CourseCard.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const CourseCardContainer = styled.div`
   width: 300px;
   height: 200px;
   aspect-ratio: calc(263/197);
-  background-color: #e3f2fd;
+  
   border-radius: 10px;
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
   display: flex;
@@ -14,45 +14,66 @@ const CourseCardContainer = styled.div`
   justify-content: center;
   position: relative;
   margin: 10px;
+
+  svg{
+    position: absolute;
+    z-index: 100;
+    top: 10px;
+    right: 10px;
+    text-decoration: none;
+    display: inline-block;
+    user-select: none;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  .change {
+    fill: #FFFF55;
+  }
 `;
 
 const CourseImage = styled.img`
   width: 100%;
   height: 100%;
-  margin-bottom: 10px;
+  object-fit: cover;
+  border-radius: 10px;
 `;
 
 const CourseTitle = styled.div`
   font-size: 18px;
-  font-weight: bold;
-  color: #333;
-  padding: 0 0 10px 0;
-`;
-
-const StarIcon = styled.button`
+  width: 100%;
+  text-align: center;
+  
+  color: #fff;
+  padding: 12px;
   position: absolute;
-  top: 10px;
-  right: 10px;
-  width: 20px;
-  height: 20px;
-  background-color: rgba(255, 255, 255, 0);
-  /* background-color: yellow; */
-  /* border-radius: 50%; */
-  text-decoration: none;
-  display: inline-block;
-  font-size: 14px;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
 `;
 
-class CourseCard extends Component {
-  render() {
-    return (
-      <CourseCardContainer>
-        <StarIcon>★</StarIcon>
-        <CourseImage src='images/loginbg.png' alt="課程圖片" />
-        <CourseTitle>軟體工程</CourseTitle>
-      </CourseCardContainer>
-    );
-  }
+
+const CourseCard = ({ isFavorite, toggleFavorite }) => {
+  const [isFilled, setIsFilled] = useState(false);
+
+  const StarHandler = () => {
+    setIsFilled(!isFilled);
+  };
+  return (
+    <CourseCardContainer>
+      {isFilled ? (
+        // 實心星星
+        <svg onClick={StarHandler} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFF55"><path d="m233-120 65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"/></svg>
+      ) : (
+        // 空心星星
+        <svg onClick={StarHandler} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#434343"><path d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-350Z"/></svg>
+      )}
+    <CourseImage src='images/c1.png' alt="課程圖片" />
+      <CourseTitle>軟體工程</CourseTitle>
+    </CourseCardContainer>
+  );
 }
+
 
 export default CourseCard;
--- my-app/src/Pages/Course.js
+++ my-app/src/Pages/Course.js
@@ -1,29 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import CourseCard from '../Components/CourseCard';
 const FlexCard = styled.div`
   display: flex;
-  width: 100%;
+  width: 100vw;
+  justify-content: center;
+  flex-wrap: wrap;
 `; 
 const Searchbar = styled.div`
   margin: 20px;
   display: flex;
   .search-bar {
-  display: flex;
-  align-items: center;
-  border: 1px solid #ccc;
-  border-radius: 30px;
-  padding: 5px 10px;
-  background-color: #fff;
-  width: 100%;
-  height: 50px;
+    display: flex;
+    align-items: center;
+    border: 1px solid #ccc;
+    border-radius: 30px;
+    padding: 5px 10px;
+    background-color: #fff;
+    width: 100%;
+    height: 50px;
   /* max-width: 500px; */
 
   input {
     border: none;
     outline: none;
     flex: 1;
-    padding: 0 10px;
+    padding: 0 15px;
     font-size: 16px;
   }
 
@@ -34,57 +36,91 @@ const Searchbar = styled.div`
   }
 
   .search-icon {
-    margin-right: 10px;
+    margin-left: 10px;
   }
 
   .filter-icon {
-    margin-left: 10px;
+    margin-right: 10px;
   }
 }
 
 `;
-// const classes = [
-//   {
-//     id: 1,
-//     name: "Mathematics",
-//     teacher: "Mr. Chen",
-//     weekday: "Monday",
-//     semester: "2024 Spring",
-//     archive: false
-//   },
-//   {
-//     id: 2,
-//     name: "Physics",
-//     teacher: "Mrs. Wang",
-//     weekday: "Wednesday",
-//     semester: "2024 Spring",
-//     archive: false
-//   },
-//   {
-//     id: 3,
-//     name: "Chemistry",
-//     teacher: "Mr. Li",
-//     weekday: "Friday",
-//     semester: "2024 Fall",
-//     archive: true
-//   }
-// ];
+
 
 const Course = () => {
+  const [classes, setClasses] = useState([
+    {
+      id: 1,
+      name: "Mathematics",
+      teacher: "Mr. Chen",
+      weekday: "Monday",
+      semester: "2024 Spring",
+      archive: false,
+      isFavorite: false,
+      image: "https://images.unsplash.com/photo-1612838320302-4b3b3b3b3b3b",
+      order: 1
+    },
+    {
+      id: 2,
+      name: "Physics",
+      teacher: "Mrs. Wang",
+      weekday: "Wednesday",
+      semester: "2024 Spring",
+      archive: false,
+      isFavorite: false,
+      image: "https://images.unsplash.com/photo-1612838320302-4b3b3b3b3b3b",
+      order: 2
+    },
+    {
+      id: 3,
+      name: "Chemistry",
+      teacher: "Mr. Li",
+      weekday: "Friday",
+      semester: "2024 Fall",
+      archive: true,
+      isFavorite: false,
+      image: "https://images.unsplash.com/photo-1612838320302-4b3b3b3b3b3b",
+      order: 3
+    },
+    {
+      id: 4,
+      name: "Biology",
+      teacher: "Mrs. Chang",
+      weekday: "Tuesday",
+      semester: "2024 Fall",
+      archive: true,
+      isFavorite: false,
+      image: "https://images.unsplash.com/photo-1612838320302-4b3b3b3b3b3b",
+      order: 4
+    }
+  ]);
+  const toggleFavorite = (index) => {
+    setClasses(classes.map((item, i) => {
+      if (i === index) {
+        return { ...item, isFavorite: !item.isFavorite };
+      }
+      return item;
+    }));
+  };
   return (
     <div>
         {/* search bar */}
         <Searchbar>
         <div class="search-bar">
-          <span class="material-symbols-outlined">search</span>
+          <span className="material-symbols-outlined search-icon">search</span>
           <input type="text" placeholder="搜尋課程" />
-          <span class="material-symbols-outlined">filter_alt</span>
+          {/* filter */}
+          <svg className='filter-icon' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="6E6E6E"><path d="M440-160q-17 0-28.5-11.5T400-200v-240L168-736q-15-20-4.5-42t36.5-22h560q26 0 36.5 22t-4.5 42L560-440v240q0 17-11.5 28.5T520-160h-80Z"/></svg>
         </div>
         </Searchbar>
         <FlexCard>
-            <CourseCard />
-            <CourseCard />
-            <CourseCard />
+          {classes.map(({ id, isFavorite, order }, index) => (
+            <CourseCard 
+              key={id}
+              isFavorite={isFavorite}
+              toggleFavorite={() => toggleFavorite(index)}
+            />
+          ))}
         </FlexCard>
     </div>
   )
