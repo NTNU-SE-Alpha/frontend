8a8e03c0f651d38d35ba5e65981f3b950f0cda81
maggiey89 <maggieyang0809@gmail.com>
2024-10-29 21:00:49 +0800

    Update Software.js

--- my-app/src/Pages/Software.js
+++ my-app/src/Pages/Software.js
@@ -6,7 +6,7 @@ const SoftwareContainer = styled.div`
   height: 100vh;
   background: #FFFFFF;
 
-  .left {
+  .left-box {
     flex: 1;
     display: flex;
     flex-direction: column;
@@ -23,7 +23,7 @@ const SoftwareContainer = styled.div`
   }
 
 
-  .right {
+  .right-box {
     flex: 9;
     display: flex;
     flex-direction: column;
@@ -34,7 +34,7 @@ const SoftwareContainer = styled.div`
     margin: 30px 30px 30px 130px;
     border-radius: 30px;
 
-    .Software-box {
+    .course-box {
       flex: 9;
       display: flex;
       flex-direction: column;
@@ -47,12 +47,16 @@ const SoftwareContainer = styled.div`
         margin: 20px;
       }
 
+      .material-symbols-outlined {
+        color: black;
+      }
+
       .dropdown {
         display: flex;
         justify-content: center;
         align-items: center;
-        height: 50px;
-        margin: 50px 50px 0px 50px;
+        height: 80px;
+        margin: 20px 50px 0px 50px;
         background: #D9D9D9;
         border-radius: 10px;
 
@@ -62,8 +66,9 @@ const SoftwareContainer = styled.div`
           
       }
 
-      .grid {
+      .grid-box {
         display: flex;
+        flex-direction: column;
         justify-content: center;
         align-items: center;
         margin: 20px 50px;
@@ -71,8 +76,60 @@ const SoftwareContainer = styled.div`
         background: #FFFFFF;
         border-radius: 10px;
         border: 2px solid black; 
+        padding: 15px;
+
+        h3 {
+          font-size: 18px;
+          display: flex;
+          justify-content: space-between;
+          width: 100%; 
+          padding-bottom: 10px;
+        }
+          
       }
-    }
+
+      .left {
+        text-align: left;
+      }
+      .center {
+        text-align: center;
+      }
+      .right {
+        text-align: right;
+      }          
+
+      .scrollable-container {
+        
+        width: 97%;               /* Set the container width */
+        height: 400px;              /* Set the container height */
+        border-radius: 5px;
+        
+        padding: 15px;              /* Optional: Padding inside container */
+        overflow-y: auto;           /* Enables vertical scrolling */
+        overflow-x: hidden;         /* Disables horizontal scrolling */
+        background-color: #D9D9D9;
+      }
+
+      .grid {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr); /* Creates 3 equal-width columns */
+        gap: 15px;                              /* Space between grid items */
+      }
+
+      .grid-item {
+        background-color: #FFFFFF;
+        color: black;
+        padding: 100px;
+        text-align: center;
+        border-radius: 20px;                   
+      }
+
+      .left-item {
+        margin-right: 10px;
+      }
+      .right-item {
+        margin-left: 500px; 
+      }
 
   }
 
@@ -81,20 +138,15 @@ const SoftwareContainer = styled.div`
 const Software = () => {
     return (
       <SoftwareContainer>
-          {/*
-          <section className='left'>
-            <button className='Softwareimage'> 
-              <img src="./images/ntnulogo.png"></img>
-            </button>
-          </section-->
-          */}
-          <section className='right'>
-            <div className='Software-box'>
 
+          <section className='right-box'>
+            <div className='course-box'>
               <h1>
-                <span class="material-symbols-outlined">
-                  arrow_back
-                </span>
+                <a href="/course">
+                  <span class="material-symbols-outlined">
+                    arrow_back
+                  </span>
+                </a>
                 &nbsp;&nbsp;軟體工程（星期二 34，四 4）
               </h1>
 
@@ -104,9 +156,46 @@ const Software = () => {
                 </h2>
               </div>
 
-              <div class='grid'>
+              <div class='grid-box'>
+                <h3>
+                  <div class="left">
+                    <a href="/course">
+                      <span class="material-symbols-outlined">
+                        arrow_back
+                      </span>
+                    </a>
+                    &nbsp;&nbsp;上一則
+                  </div>
+                  
+                  <div class="center">第一週</div>
 
+                  <div class="right">
+                    下一則&nbsp;&nbsp;
+                    <a href="/course">
+                      <span class="material-symbols-outlined">
+                      arrow_forward
+                      </span>
+                    </a>
+                  </div>       
+                </h3>
+
+                <div class='scrollable-container'>
+                  
+                  <span class="material-symbols-outlined right-item">
+                    edit_square
+                  </span>
+                  
+                  <div class="grid">
+                    <div class="grid-item">作業</div>
+                    <div class="grid-item">課程資訊</div>
+                    <div class="grid-item">考試</div>
+                    <div class="grid-item"> </div>
+                    <div class="grid-item"> </div>
+                    <div class="grid-item"> </div>
+                  </div>
+                </div>
               </div>
+
             </div>
           </section>
       </SoftwareContainer>
